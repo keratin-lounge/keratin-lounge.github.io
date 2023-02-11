6706a45270e92f45bd9c594e2d7adbeaff65597d
keratin-lounge <124483696+keratin-lounge@users.noreply.github.com>
2023-02-10 21:22:45 -0600

    Add files via upload

---
+++ Yeva-site/js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $('.your-class').slick({
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    centerMode: true,
+    variableWidth: true,
+    autoplay: true,
+    autoplaySpeed: 2000
+  });
+});
